8c5050dfe09d0d94a246b15b4b3417ba98a49041
Rahul Chauhan <chauhan-rahul@hotmail.com>
2024-08-01 19:58:12 +1000

    Unused memberdata removed

--- CodingPad.Tests/FileInfoTests.cs
+++ CodingPad.Tests/FileInfoTests.cs
@@ -9,7 +9,6 @@ using Xunit;
 
 namespace CodingPad.Tests
 {
-
     public class FileDataObject : TheoryData<int?, List<FileData>>
     {
         public FileDataObject()
@@ -37,61 +36,20 @@ namespace CodingPad.Tests
 
     public class FileInfoTests
     {
-        public static IEnumerable<object[]> GetFileDataList()
+        private readonly FileInfo _sut;
+
+        public FileInfoTests()
         {
-            return new List<object[]>
-            {
-               new object[]
-               {
-                   new List<FileData>
-                    {
-                        new FileData
-                        {
-                             Name = "file1.txt",
-                             Size = 100,
-                             Collections = new HashSet<string>{ "collection4" }
-                        },
-                        new FileData
-                        {
-                            Name = "file2.txt",
-                            Size = 200,
-                            Collections = new HashSet<string>{ "collection1" }
-                        },
-                        new FileData { Name = "file3.txt", Size = 200, Collections = new HashSet<string>{ "collection1" } },
-                        new FileData { Name = "file4.txt", Size = 300, Collections = new HashSet<string>{ "collection2", "collection3" } },
-                        new FileData { Name = "file5.txt", Size = 10, Collections = new HashSet<string>() },
-                    }
-               }
-            };
+            _sut = new FileInfo();
         }
 
         [Theory]
         [ClassData(typeof(FileDataObject))]
         public void GetTopN_Should_ReturnTopNCollectionInDescendingCollectionSizeOrder_WhenGivenAListOfFiles(int n, List<FileData> fileDatas)
         {
-            var result = fileDatas
-                        .SelectMany(
-                            fileData => fileData.Collections,
-                            (fileData, collection) => new
-                            {
-                                FileData = fileData,
-                                Collection = collection
-                            }
-                        )
-                        .GroupBy(
-                            item => item.Collection,
-                            item => item.FileData.Size,
-                            (collection, sizes) => new CollectionData
-                            {
-                                Name = collection,
-                                Size = sizes.Sum()
-                            }
-                        )
-                        .OrderByDescending(data => data.Size)
-                        .Take(n)
-                        .ToList();
+            var topNCollection = _sut.GetTopNCollections(n, fileDatas).ToList();
 
-            Assert.Equal(3, result.Count);
+            Assert.Equal(3, topNCollection.Count);
         }
 
         [Theory]
@@ -99,8 +57,7 @@ namespace CodingPad.Tests
         public void GetTotalSize_Should_ReturnSizeOfAllFilesInAllCollections_WhenGivenAListOfFiles(int n, List<FileData> fileDatas)
         {
             int expectedSize = 810;
-            var result = fileDatas
-                        .Sum(item => item.Size);
+            var result = _sut.GetTotalSize(fileDatas);
 
             Assert.Equal(expectedSize, result);
         }
